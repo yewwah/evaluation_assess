13acd1bd7a1419bc9d6bac7532ac299bc7e489b7
abc <abc>
2024-03-08 11:10:55 +0800

    wip

--- cv-decode.py
+++ cv-decode.py
@@ -3,24 +3,25 @@ from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC, pipeline
 import torch
 from tqdm import tqdm
 import librosa
-# from tqdm.auto import tqdm  # for notebooks
 
-# Create new `pandas` methods which use `tqdm` progress
-# (can use tqdm_gui, optional kwargs, etc.)
+# Progress report 
 tqdm.pandas()
  
  # load model and processor
 
+# Pipeline initialization
 pipe = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-large-960h",  chunk_length_s = 10, stride_length_s=(4,2))
-# load dummy dataset and read soundfiles
+
+# Transcribing based on zip files
 def transcribe(fname : str) -> str:
     print('cv-valid-dev/' + fname)
+    # Loading the audio
     audio, _ = librosa.load('cv-valid-dev/' + fname, sr = 16000)
-    # tokenize
+    # returning the result 
     return pipe(audio)['text']
     
 
 df = pd.read_csv('cv-valid-dev.csv')
 df['generated_text'] = df['filename'].progress_apply(transcribe)
-df.to_csv('cv-valid-dev-generated.csv', index=False)
+df.to_csv('cv-valid-dev.csv', index=False)
 
