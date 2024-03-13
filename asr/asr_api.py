062148f052c3a544a099dc8940c6bb9a018383ff
abc <abc>
2024-03-13 16:25:34 +0800

    wip

--- asr/asr_api.py
+++ asr/asr_api.py
@@ -1,21 +1,16 @@
-from flask import Flask, request
-from typing import Dict, List
+from typing import Dict
+from fastapi import FastAPI, Form
 
-import json
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/', methods=['GET'])
+@app.get('/')
 def ping() -> str:
     return 'pong'
 
-@app.route('/asr', methods=['POST'])
-def asr() -> Dict[str, str]: 
-    fname = request.form['file']
+@app.post('/asr')
+async def create_item(file: str = Form(...)):
+    print(file)
     return {
         "transcription": "BEFORE HE HAD TIME TO ANSWER A MUCH ENCUMBERED VERA BURST INTO THE ROOM",
         "duration" : "20.7"
     }
-
-
-if __name__ == '__main__':
-   app.run(port=8001)
